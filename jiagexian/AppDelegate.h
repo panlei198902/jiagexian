080e7c037797fba33811c457a0a232f2c28fe57c
derex pan <derexpan@derexdembp.lan>
2016-11-27 16:54:03 +0800

    Initial Commit

---
+++ jiagexian/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  jiagexian
+//
+//  Created by derex pan on 2016/11/27.
+//  Copyright © 2016年 derex pan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ jiagexian/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  jiagexian
+//
+//  Created by derex pan on 2016/11/27.
+//  Copyright © 2016年 derex pan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
